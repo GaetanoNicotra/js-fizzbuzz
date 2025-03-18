4c9502c4c6d8986c6c4f89faa08e45f2fd7adf94
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-18 16:06:11 +0100

    [UPD]

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@
 // inserisco le condizioni da rispettare affinchè il ciclo for sia verificato
 // stampo in console il risultato
 
+let pippo = ' ';
+
 for (let i=1; i<=100; i++){
     console.log(i)
+
+    if(i % 3 == 0){
+        console.log(`Fizz`)
+    }
+
+    if(i % 5 == 0)
+        console.log(`Buzz`)
 }
